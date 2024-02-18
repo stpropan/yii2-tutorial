fd2b2e1e1b83326466c03fc46016e22191975531
stpropan <st.propan@gmail.com>
2024-02-19 01:17:27 +0300

    Добавление нового поля и правила его валидации

--- models/UserRegister.php
+++ models/UserRegister.php
@@ -4,4 +4,18 @@ namespace app\models;
 
 class UserRegister extends User
 {
+    public $password_confirmation = '';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return array_merge (
+            parent::rules(),
+            [
+                ['password_confirmation', 'compare', 'compareAttribute' => 'password']
+            ]
+        );
+    }
 }
