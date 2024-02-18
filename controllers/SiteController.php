d327084edce8d5316201589dc775487ba774e75d
stpropan <st.propan@gmail.com>
2024-02-19 01:45:14 +0300

    Редактируем название и функционал метода регистрации

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -9,6 +9,7 @@ use yii\web\Response;
 use yii\filters\VerbFilter;
 use app\models\LoginForm;
 use app\models\ContactForm;
+use app\models\Role;
 use app\models\User;
 
 class SiteController extends Controller
@@ -132,13 +133,20 @@ class SiteController extends Controller
      * If creation is successful, the browser will be redirected to the 'view' page.
      * @return string|\yii\web\Response
      */
-    public function actionCreate()
+    public function actionRegister()
     {
         $model = new User();
 
         if ($this->request->isPost) {
-            if ($model->load($this->request->post()) && $model->save()) {
-                return $this->redirect(['view', 'id' => $model->id]);
+            // Можно поставить $model->validate(), для проверки валидации, но она автоматически проверяется методом $model->save()
+            if ($model->load($this->request->post())) {
+                // Назначаем новому пользователю id роли пользователя
+                $model->role_id = Role::USER_ROLE_ID;
+                // Переносим сохранение модели в отдельное условие, чтобы была возможность добавить id роли до сохранения
+                if ($model->save()) {
+                    // Переносим пользователя на страницу аутентификации при успешном созранении модели
+                    return $this->redirect('/site/login');
+                }
             }
         } else {
             $model->loadDefaultValues();
